b50d4705f4903ca2855433e71535932fd37bb8b1
Dirkstar <info@smartsys.de>
2026-08-28 09:16:43 +0200

    Bonus LI eigebaut

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -7,41 +7,41 @@ def load_data(file_path):
         return json.load(handle)
 
 
-animals_data = load_data('animals_data.json')
-print(animals_data)
-
-output = ''
-
-for animal in animals_data:
+def serialize_animal(animal):
     name = animal.get('name', 'Unbekannt')
     locations = animal.get('locations', [])[0]
     characteristics = animal.get('characteristics', {})
     diet = characteristics.get('diet', 'N/A')
     animal_type = characteristics.get('type', 'N/A')
+    animal_temperament = characteristics.get('temperament', 'N/A')
+    animal_skin_type = characteristics.get('skin_type', 'N/A')
 
-    # Ausgabe
-    # print("Name:", name)
-    # print("Diet:", diet)
-    # print("Location:", locations)
-    # print("Type:", animal_type)
-    # print()
-    output += '<li class="cards__item">'
+    output = '<li class="cards__item">'
     output += f"<div class=\"card__title\">{name}</div>"
-    output += f"<p class =\"card__text\" >"
-    output += f"<strong>Diet:</strong> {diet}<br/>"
-    output += f"<strong>Location:</strong> {locations}<br/>"
-    output += f"<strong>Type:</strong> {animal_type}<br/>"
-
-    output += "</p>"
-
+    output += f"<div class =\"card__text\" >"
+    output += f"<ul>"
+    output += f"<li><strong>Diet:</strong> {diet}</li>"
+    output += f"<li><strong>Location:</strong> {locations}</li>"
+    output += f"<li><strong>Type:</strong> {animal_type}</li>"
+    output += f"<li><strong>Temperament:</strong> {animal_temperament}</li>"
+    output += f"<li><strong>Skin type:</strong> {animal_skin_type}</li>"
+    output += f"</ul>"
+    output += "</div>"
     output += '</li>'
-print(output)
+
+    return output
+
+
+animals_data = load_data('animals_data.json')
+
+output = ''
+for animal in animals_data:
+    output += serialize_animal(animal)
 
 with open('animals_template.html', 'r') as file:
     template = file.read()
 
 template = template.replace('__REPLACE_ANIMALS_INFO__', output)
-print(template)
 
 with open('animals.html', 'w') as file:
     file.write(template)
